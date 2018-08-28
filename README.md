d1b8d21c7c5897e304017f991b21f32991b71624
iso-code <81surf05>
2018-08-28 20:02:51 +0200

    changed description
    Marcel Gaj

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# LGR-DLT100
+# This script enables to use the LGR DLT-100 in automatic mode without attached Autosampler
+# instead you create a connection between your serial to usb adapter and the LGR
+# Things to do:
+# - Configure your serial port
+# - make a bridge between pin 4 and 7 of at the LGR serial plug
+# - Connect the serial of your computer and the LGR
+# - start the script, then start a measurement cycle with the LGR
+
+# A applied example can be found here:
+# https://doi.org/10.5194/hess-20-715-2016
--- mimic_pal_sampler_DLT-100.py
+++ mimic_pal_sampler_DLT-100.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
 
-# This script enables to use the LGR DLT-100 in automatic mode without attached Autosampler
-# instead you create a connection between your serial to usb adapter and the LGR
-# Things to do:
-# - Configure your serial port
-# - make a bridge between pin 4 and 7 of at the LGR serial plug
-# - Connect the serial of your computer and the LGR
-# - start the script, then start a measurement cycle with the LGR
-
-# A applied example can be found here:
-# https://doi.org/10.5194/hess-20-715-2016
-
 __author__ = "Marcel Gaj"
 __copyright__ = "Copyright 2018"
 __credits__ = [""]
